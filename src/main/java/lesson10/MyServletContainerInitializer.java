0130c66020f14705e01ab13a03a58a88cb622df0
bruce <bruce>
2023-06-01 15:17:16 +0800

    add comment

--- src/main/java/lesson10/MyServletContainerInitializer.java
+++ src/main/java/lesson10/MyServletContainerInitializer.java
@@ -6,6 +6,12 @@ import javax.servlet.ServletException;
 import javax.servlet.annotation.HandlesTypes;
 import java.util.Set;
 
+/**
+ * servlet SPI 機制：啟動時，會去加載
+ * resources/META-INF/services/
+ * javax.servlet.ServletContainerInitializer 的檔案
+ * 裡面只要寫 ServletContainerInitializer 實作內容即可
+ */
 @HandlesTypes(GrandPapa.class) // GrandPapa 不會被加載，但子類，不管是介面、抽象類別、類別都會被加載
 public class MyServletContainerInitializer implements ServletContainerInitializer {
     @Override
